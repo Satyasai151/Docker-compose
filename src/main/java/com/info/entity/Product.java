f8db3d8d85b3235e32d901f4834b505320ba8a2c
Satyasai151 <balijepallisatyasai151@gmail.com>
2023-07-12 12:59:47 +0530

    first commit

--- src/main/java/com/info/entity/Product.java
+++ src/main/java/com/info/entity/Product.java
@@ -9,10 +9,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 @Entity
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Product {
 	@Id
 	@GeneratedValue
@@ -22,6 +18,58 @@ public class Product {
 	private String prodVender;
 	private Double prodGst;
 	private Double prodDis;
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Product(Integer prodId, String prodCode, Double prodCost, String prodVender, Double prodGst,
+			Double prodDis) {
+		super();
+		this.prodId = prodId;
+		this.prodCode = prodCode;
+		this.prodCost = prodCost;
+		this.prodVender = prodVender;
+		this.prodGst = prodGst;
+		this.prodDis = prodDis;
+	}
+	public Integer getProdId() {
+		return prodId;
+	}
+	public void setProdId(Integer prodId) {
+		this.prodId = prodId;
+	}
+	public String getProdCode() {
+		return prodCode;
+	}
+	public void setProdCode(String prodCode) {
+		this.prodCode = prodCode;
+	}
+	public Double getProdCost() {
+		return prodCost;
+	}
+	public void setProdCost(Double prodCost) {
+		this.prodCost = prodCost;
+	}
+	public String getProdVender() {
+		return prodVender;
+	}
+	public void setProdVender(String prodVender) {
+		this.prodVender = prodVender;
+	}
+	public Double getProdGst() {
+		return prodGst;
+	}
+	public void setProdGst(Double prodGst) {
+		this.prodGst = prodGst;
+	}
+	public Double getProdDis() {
+		return prodDis;
+	}
+	public void setProdDis(Double prodDis) {
+		this.prodDis = prodDis;
+	}
+	
+	
 
 
 }
